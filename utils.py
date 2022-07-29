2cf64ab235fb97847f233198487eb6c96f00eb46
NoahGWood <NoahGWood@users.noreply.github.com>
2022-07-29 16:56:22 -0400

    Added presentation viewer, markdown editor

---
+++ utils.py
@@ -0,0 +1,83 @@
+from database import db_session
+from models import User, File, Presentation, PresentationUser
+from file_manager import load_file_text, upload_file_from_txt
+
+def FindPresentationFile(presentation):
+    return db_session.query(File).filter(
+        File.filepath.contains(presentation+'.txt')
+        ).first()
+
+def FindAllPresentationAudio(presentation):
+    audios = []
+    for pf in presentation.files:
+        if 'audio' in pf.filetype:
+            audios.append(pf)
+    return audios
+
+def FindAudioBySlide(audio_list, slide):
+    """Returns the file for an approriate slide."""
+    for each in audio_list:
+        if 'audio{}'.format(slide) in each.filetype:
+            return each
+    return None
+    
+def FindPresentationOwnerByFile(pfile):
+    return db_session.query(PresentationUser).filter(
+        PresentationUser.presentation_id == pfile.presentation_id
+        ).first()
+
+def GetPresentationByFile(pfile):
+    return db_session.query(Presentation).filter(
+        Presentation.id == pfile.presentation_id
+        ).first()
+
+
+def UserCanAccessPresentation(user, pres_user):
+    """Returns true if user can access presentation, false if not."""
+    return user.id == pres_user.user_id
+
+def AuthPresentation(user, presentation):
+    """Handles authenticating user to presentation.
+    
+        Returns tuple (true, presentation, main file) if available
+    """
+    pres_file = FindPresentationFile(presentation)
+    pres_user = FindPresentationOwnerByFile(pres_file)
+    pres = GetPresentationByFile(pres_file)
+    return UserCanAccessPresentation(user, pres_user), pres, pres_file
+
+
+def NewPresentation(title, language, translate, genimages, text):
+    fname = str(uuid.uuid4())
+    write_text_file(fname, text)
+    time = datetime.datetime.now()
+    p = Presentation(title=title, ctime=time, mtime=time,
+                     translate=translate, genimages=genimages)
+    f = File(language=language, ctime=time, mtime=time,
+             filepath="{}.txt".format(fname))
+    p.files.append(f)
+    curr_usr = db_session.query(User).filter(
+        User.id == current_user.id).first()
+    curr_usr.presentations.append(p)
+    db_session.add(p)
+    db_session.add(f)
+    db_session.add(curr_usr)
+    db_session.commit()
+    return fname
+
+def write_text_file(fname, text):
+    """ Writes text to file fname"""
+    upload_file_from_txt(fname+'.txt', text)
+
+
+def GetText(filepath):
+    """Returns the text of file"""
+    txt = ''
+    return load_file_text(filepath)
+
+def GetVideos(presentation):
+    vids = []
+    for f in presentation.files:
+        if f.ftype == 'video':
+            vids.append(f)
+    return vids
--- views/editor.py
+++ views/editor.py
@@ -2,30 +2,19 @@ import os
 from flask import Blueprint, flash, render_template, request, url_for, current_app, abort
 from flask_security import current_user, auth_required
 from werkzeug.utils import redirect
-from models import User, File, Presentation, PresentationUser
 from database import db_session
-from file_manager import load_file_text, upload_file_from_txt
+from utils import *
 import datetime
 import hashlib
 import uuid
 
 editor_pages = Blueprint("editor_pages", __name__, url_prefix='/editor')
 
-
 @editor_pages.route("/<presentation>", methods=["GET", "POST"])
 @auth_required()
 def edit(presentation):
-    #    presentation=presentation.split('?')[0]
-    a = db_session.query(File).filter(
-        File.filepath.contains(presentation+'.txt')
-    ).first()
-    pres_user = db_session.query(PresentationUser).filter(
-        PresentationUser.presentation_id == a.presentation_id
-    ).first()
-    pres = db_session.query(Presentation).filter(
-        Presentation.id == a.presentation_id
-    ).first()
-    if current_user.id == pres_user.user_id:
+    access, pres, pres_file = AuthPresentation(current_user, presentation)
+    if access:
         if request.method == "POST":
             txt = request.form['text']
             # Make sure text is not null
@@ -36,8 +25,8 @@ def edit(presentation):
             return redirect("/editor/{}".format(presentation))
         else:
             videos = GetVideos(pres)
-            print(a.filepath)
-            text = GetText(a.filepath)
+            print(pres_file.filepath)
+            text = GetText(pres_file.filepath)
             return render_template("editor.html", name=current_user.email, current=pres, videos=videos, text=text.replace("\n","\\n").replace("\r","\\r"), fname=presentation)
     else:
         return redirect("/editor/new")
@@ -70,52 +59,6 @@ def new():
 @auth_required()
 def submitted():
     return render_template("submitted.html")
-
-
-def NewPresentation(title, language, translate, genimages, text):
-    fname = str(uuid.uuid4())
-    write_text_file(fname, text)
-    time = datetime.datetime.now()
-    p = Presentation(title=title, ctime=time, mtime=time,
-                     translate=translate, genimages=genimages)
-    f = File(language=language, ctime=time, mtime=time,
-             filepath="{}.txt".format(fname))
-    p.files.append(f)
-    curr_usr = db_session.query(User).filter(
-        User.id == current_user.id).first()
-    curr_usr.presentations.append(p)
-    db_session.add(p)
-    db_session.add(f)
-    db_session.add(curr_usr)
-    db_session.commit()
-    return fname
-
-
-def write_text_file(fname, text):
-    """ Writes text to file fname"""
-    # Save text to file
-    upload_file_from_txt(fname+'.txt', text)
-#    print(fname, text)
-#    with open('static/uploads/{}.txt'.format(fname), 'w') as f:
-#        f.writelines(text)
-
-
-def GetText(filepath):
-    """Returns the text of file"""
-    txt = ''
-    return load_file_text(filepath)
-#    with open(filepath, 'r') as f:
-#        for line in f.readlines():
-#            txt += line
-#    return txt
-
-
-def GetVideos(presentation):
-    vids = []
-    for f in presentation.files:
-        if f.ftype == 'video':
-            vids.append(f)
-    return vids
 
 @editor_pages.route("/video/<video_id>", methods=["GET"])
 @auth_required()
